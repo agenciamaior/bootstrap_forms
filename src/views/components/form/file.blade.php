e7070a0e3a52284d040b622c37c188ffd8023a19
Desenvolvedor <desenvolvedor@Mac-mini-de-Desenvolvedor.local>
2019-03-01 11:36:15 -0300

    IDs dos campos

--- src/views/components/form/file.blade.php
+++ src/views/components/form/file.blade.php
@@ -1,5 +1,7 @@
 @php
     $class = (empty($attributes['class'])) ? 'form-control' : 'form-control ' . $attributes['class'];
+    
+    $attributes['id'] = (!empty($attributes['id'])) ? $attributes['id'] : $name;
 @endphp
 
 <div class="form-group">
