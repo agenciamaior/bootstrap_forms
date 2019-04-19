51e77866f50062ec3998d206511e2cc8c667f70e
Alexandre <alexandre.sottani@agenciamaior.com.br>
2019-04-19 16:10:37 -0300

    Atributos do submit

--- src/views/components/form/submit.blade.php
+++ src/views/components/form/submit.blade.php
@@ -1,22 +1,28 @@
 @php
-    $class = (!empty($attributes['class'])) ? $attributes['class'] : 'btn btn-success btn-lg';
-    $id = !empty($attributes['id']) ? $attributes['id'] : '';
+    $attributes['class'] = (!empty($attributes['class'])) ? $attributes['class'] : 'btn btn-success btn-lg';
+    $attributes['id'] = !empty($attributes['id']) ? $attributes['id'] : '';
+    $attributes['type'] = !empty($attributes['type']) ? $attributes['type'] : 'submit';
+
+    $hasIcon = true;
+    $icon = 'check';
+
+    if (array_key_exists('icon', $attributes)) {
+        if ($attributes['icon'] == null) {
+            $hasIcon = false;
+        } else {
+            $icon = $attributes['icon'];
+        }
+    }
+
+    unset($attributes['icon']);
+
+    $buffer = [];
+    foreach ($attributes as $key => $a) {
+        $buffer[] = $key . '="' . $a . '"';
+    }
 @endphp
 
-<button type="submit" class="{{ $class }}" id="{{ $id }}">
-    @php
-        $hasIcon = true;
-        $icon = 'check';
-
-        if (array_key_exists('icon', $attributes)) {
-            if ($attributes['icon'] == null) {
-                $hasIcon = false;
-            } else {
-                $icon = $attributes['icon'];
-            }
-        }
-    @endphp
-
+<button {!! implode(' ', $buffer) !!}>
     @if($hasIcon)
         <i class="fa fa-{{ $icon }}"></i> 
     @endif
